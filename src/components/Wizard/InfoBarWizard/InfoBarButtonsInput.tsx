2a08afd6411ca319fad91d5000aa5b560575be4c
Barret Rennie <barret@brennie.ca>
2022-11-28 17:17:23 -0500

    Split out the tab control from buttons input
    THe tabbed portion of the InfoBarButtonsInput is now its own component,
    the TabbedInput. This component will be re-used for editing Spotlight
    screens.

--- src/components/Wizard/InfoBarWizard/InfoBarButtonsInput.tsx
+++ src/components/Wizard/InfoBarWizard/InfoBarButtonsInput.tsx
@@ -4,16 +4,12 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import { useState } from "react";
 import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
-import Nav from "react-bootstrap/Nav";
 import Row from "react-bootstrap/Row";
-import Tab from "react-bootstrap/Tab";
-import { faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useFieldArray, useFormContext } from "react-hook-form";
+import { useFormContext } from "react-hook-form";
 
 import FormRow from "../FormRow";
 import InfoBarWizardFormData from "./formData";
@@ -24,162 +20,103 @@ import {
 } from "../../RegisteredFormControl";
 import { validateJsonAsObject } from "../validators";
 import ErrorMessage from "../../ErrorMessage";
+import TabbedInput, { TabInputProps } from "../TabbedInput";
 
-const defaults = (): InfoBarWizardFormData["content"]["buttons"][number] => ({
-  label: {
-    localized: false,
-    text: "",
-  },
-  accessKey: "",
-  primary: false,
-  supportPage: "",
-  action: "{}",
-});
+function defaults(): InfoBarWizardFormData["content"]["buttons"][number] {
+  return {
+    label: {
+      localized: false,
+      text: "",
+    },
+    accessKey: "",
+    primary: false,
+    supportPage: "",
+    action: "{}",
+  };
+}
 
 const controlPrefix = "content.buttons";
 
-export default function InfoBarButtonsInput() {
-  const { control, register } = useFormContext<InfoBarWizardFormData>();
-  const { fields, append, remove } = useFieldArray<InfoBarWizardFormData>({
-    control,
-    name: controlPrefix,
-  });
-  // The currently active tab is stored as its index converted to a string.
-  const [activeKey, setActiveKey] = useState<string | null>(
-    fields.length ? "0" : null
-  );
+export default function InfoBaorButtonsInput() {
+  const { register } = useFormContext<InfoBarWizardFormData>();
 
-  const addButton = () => {
-    setActiveKey(fields.length.toString());
-    append(defaults(), {
-      focusName: `${controlPrefix}.${fields.length}.label.localized`,
-    });
-  };
-
-  const tabs = fields.map((field, idx) => {
+  const renderTab = ({ index, handleDelete }: TabInputProps) => {
+    const tabControlPrefix = `${controlPrefix}.${index}` as const;
     return (
-      <Nav.Item key={field.id}>
-        <Nav.Link eventKey={idx}>{idx}</Nav.Link>
-      </Nav.Item>
-    );
-  });
+      <>
+        <LocalizableTextInput
+          controlPrefix={`${tabControlPrefix}.label`}
+          label="Label"
+          required
+        />
 
-  const tabContents = fields.length
-    ? fields.map((field, idx) => {
-        const tabControlPrefix = `${controlPrefix}.${idx}` as const;
-        const onDelete = () => {
-          if (activeKey && activeKey === String(idx)) {
-            if (idx > 0) {
-              setActiveKey((idx - 1).toString());
-            } else if (idx === 0 && fields.length === 1) {
-              setActiveKey(null);
-            }
-            // If we are deleting the first button and there is another button, we
-            // just stay at the same button and therefore don't have to change the
-            // activeKey.
-          }
-          remove(idx);
-        };
-
-        return (
-          <Tab.Pane eventKey={idx} key={field.id} as="fieldset">
-            <LocalizableTextInput
-              controlPrefix={`${tabControlPrefix}.label`}
-              label="Label"
-              required
+        <FormRow label="Access Key" controlId={`${controlPrefix}.accessKey`}>
+          <div className="access-key-input">
+            <RegisteredFormControl
+              name={`${tabControlPrefix}.accessKey`}
+              register={register}
+              type="text"
+              maxLength={1}
             />
+          </div>
+        </FormRow>
 
-            <FormRow
-              label="Access Key"
-              controlId={`${controlPrefix}.accessKey`}
-            >
-              <div className="access-key-input">
-                <RegisteredFormControl
-                  name={`${tabControlPrefix}.accessKey`}
-                  register={register}
-                  type="text"
-                  maxLength={1}
-                />
-              </div>
-            </FormRow>
+        <FormRow
+          label="Primary?"
+          controlId={`${controlPrefix}.primary`}
+          containerClassName="form-input-check"
+        >
+          <RegisteredFormCheck
+            name={`${tabControlPrefix}.primary`}
+            register={register}
+          />
+        </FormRow>
 
-            <FormRow
-              label="Primary?"
-              controlId={`${controlPrefix}.primary`}
-              containerClassName="form-input-check"
-            >
-              <RegisteredFormCheck
-                name={`${tabControlPrefix}.primary`}
-                register={register}
-              />
-            </FormRow>
+        <FormRow label="Support URL" controlId={`${controlPrefix}.supportPage`}>
+          <RegisteredFormControl
+            name={`${tabControlPrefix}.supportPage`}
+            register={register}
+            type="text"
+            className="input-monospace"
+          />
+        </FormRow>
 
-            <FormRow
-              label="Support URL"
-              controlId={`${controlPrefix}.supportPage`}
-            >
-              <RegisteredFormControl
-                name={`${tabControlPrefix}.supportPage`}
-                register={register}
-                type="text"
-                className="input-monospace"
-              />
-            </FormRow>
+        <FormRow label="Action" controlId={`${controlPrefix}.action`}>
+          <RegisteredFormControl
+            name={`${tabControlPrefix}.action`}
+            register={register}
+            registerOptions={{
+              required: true,
+              validate: validateJsonAsObject,
+            }}
+            as="textarea"
+            className="input-monospace"
+          />
+          <ErrorMessage name={`${tabControlPrefix}.action`} />
+        </FormRow>
 
-            <FormRow label="Action" controlId={`${controlPrefix}.action`}>
-              <RegisteredFormControl
-                name={`${tabControlPrefix}.action`}
-                register={register}
-                registerOptions={{
-                  required: true,
-                  validate: validateJsonAsObject,
-                }}
-                as="textarea"
-                className="input-monospace"
-              />
-              <ErrorMessage name={`${tabControlPrefix}.action`} />
-            </FormRow>
-
-            <Row className="form-row form-buttons">
-              <Col>
-                <Button variant="danger" onClick={onDelete}>
-                  <FontAwesomeIcon icon={faTrash} /> Delete
-                </Button>
-              </Col>
-            </Row>
-          </Tab.Pane>
-        );
-      })
-    : "There are no buttons";
+        <Row className="form-row form-buttons">
+          <Col>
+            <Button variant="danger" onClick={handleDelete}>
+              <FontAwesomeIcon icon={faTrash} /> Delete
+            </Button>
+          </Col>
+        </Row>
+      </>
+    );
+  };
 
   return (
     <FormRow label="Buttons">
-      <Card className="infobar-buttons-input">
-        <Tab.Container
-          activeKey={activeKey ?? undefined}
-          onSelect={setActiveKey}
-        >
-          <Card.Header>
-            <Nav variant="tabs">
-              {tabs}
-              <div className="tabs-controls">
-                <Nav.Item>
-                  <Button
-                    onClick={addButton}
-                    className="new-button"
-                    title="Add a button"
-                  >
-                    <FontAwesomeIcon icon={faPlus} />
-                  </Button>
-                </Nav.Item>
-              </div>
-            </Nav>
-          </Card.Header>
-          <Card.Body>
-            <Tab.Content>{tabContents}</Tab.Content>
-          </Card.Body>
-        </Tab.Container>
-      </Card>
+      <TabbedInput
+        className="infobar-buttons-input"
+        controlPrefix="content.buttons"
+        emptyTabs="There are no buttons."
+        renderTab={renderTab}
+        focusName="label.raw"
+        defaults={defaults}
+        addText="Add button"
+      />
     </FormRow>
   );
 }

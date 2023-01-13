7c4f38f831a1fa5d0b901f00d821cecb04c4145c
Barret Rennie <barret@brennie.ca>
2023-01-13 18:13:02 -0500

    Forbid unused variables with eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -35,6 +35,7 @@ module.exports = {
                 "checksVoidReturn": false
             }
         ],
+        "@typescript-eslint/no-unused-vars": "error",
         "no-console": [
             "error",
             {

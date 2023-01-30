6d2e0c256816fd984e4a783fdb9c85bb75ae503c
Barret Rennie <barret@brennie.ca>
2023-01-30 18:40:33 -0500

    Split out NewEditImportPane into a file per component

---
+++ src/components/Wizard/NewEditImportPane/JsonImportMessageForm.tsx
@@ -0,0 +1,106 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import { useForm, FormProvider, SubmitHandler } from "react-hook-form";
+
+import FormRow from "../FormRow";
+import { MessageTemplate } from "../messageTypes";
+import { RegisteredFormControl } from "../../RegisteredFormControl";
+import { validateJsonAsObject } from "../validators";
+import deserialize from "../deserializers";
+import WizardFormData from "../formData";
+import ErrorMessage from "../../ErrorMessage";
+import { useToastsContext } from "../../../hooks/useToasts";
+
+interface JsonMessageImportFormData {
+  messageJson: string;
+}
+
+export interface JsonMessageImportFormProps {
+  onImportMessage: (
+    messageId: string,
+    messageTemplate: MessageTemplate,
+    formData: WizardFormData
+  ) => void;
+}
+
+export default function JsonMessageImportForm({
+  onImportMessage,
+}: JsonMessageImportFormProps) {
+  const { addToast } = useToastsContext();
+  const formContext = useForm<JsonMessageImportFormData>({ mode: "onChange" });
+  const {
+    register,
+    handleSubmit,
+    formState: { isDirty, isValid },
+    setError,
+  } = formContext;
+
+  const onSubmit: SubmitHandler<JsonMessageImportFormData> = (
+    data: JsonMessageImportFormData
+  ) => {
+    const json = JSON.parse(data.messageJson) as Record<string, unknown>;
+    let result;
+
+    try {
+      result = deserialize(json);
+    } catch (e) {
+      setError("messageJson", {
+        message: `Could not import message: ${String(e)}`,
+      });
+      console.error(e);
+      return;
+    }
+
+    if (result.warnings.length) {
+      console.warn(`Message ${result.id} imported with warnings: `);
+      for (const { field, message } of result.warnings) {
+        console.warn(`[${field}]: ${message}`);
+      }
+
+      addToast(
+        "Message imported with warnings",
+        "See browser console for full details"
+      );
+    }
+
+    onImportMessage(result.id, result.template, result.formData);
+  };
+
+  return (
+    <>
+      <Card.Title>Import a Message</Card.Title>
+      <Form onSubmit={handleSubmit(onSubmit)} className="import-form">
+        <FormProvider {...formContext}>
+          <Form.Group controlId="message-json">
+            <FormRow label="Message JSON" controlId="messageJson">
+              <RegisteredFormControl
+                as="textarea"
+                name="messageJson"
+                register={register}
+                registerOptions={{
+                  required: true,
+                  validate: validateJsonAsObject,
+                }}
+                className="input-monospace"
+              />
+              <ErrorMessage name="messageJson" />
+            </FormRow>
+          </Form.Group>
+
+          <div className="form-row form-buttons">
+            <Button type="submit" disabled={!isValid || !isDirty}>
+              Next
+            </Button>
+          </div>
+        </FormProvider>
+      </Form>
+    </>
+  );
+}
---
+++ src/components/Wizard/NewEditImportPane/index.tsx
@@ -0,0 +1,74 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Tab from "react-bootstrap/Tab";
+
+import NewMessageForm, { NewMessageFormProps } from "./NewMessageForm";
+import EditMessageForm, { EditMessageFormProps } from "./EditMessageForm";
+import JsonMessageImportForm, {
+  JsonMessageImportFormProps,
+} from "./JsonImportMessageForm";
+
+enum EventKeys {
+  New = "new",
+  Edit = "edit",
+  Import = "import",
+}
+
+type NewEditImportPaneProps = NewMessageFormProps &
+  EditMessageFormProps &
+  JsonMessageImportFormProps;
+
+export default function NewEditImportPane({
+  onNewMessage,
+  onEditMessage,
+  onDeleteMessage,
+  onImportMessage,
+  messages,
+}: NewEditImportPaneProps) {
+  return (
+    <Container>
+      <Card className="col-lg-8 offset-lg-2 mt-3">
+        <Tab.Container defaultActiveKey={EventKeys.New}>
+          <Card.Header>
+            <Nav variant="tabs">
+              <Nav.Item>
+                <Nav.Link eventKey={EventKeys.New}>New</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey={EventKeys.Edit}>Edit</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey={EventKeys.Import}>Import</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Card.Header>
+
+          <Card.Body>
+            <Tab.Content>
+              <Tab.Pane eventKey={EventKeys.New}>
+                <NewMessageForm onNewMessage={onNewMessage} />
+              </Tab.Pane>
+              <Tab.Pane eventKey={EventKeys.Edit}>
+                <EditMessageForm
+                  onEditMessage={onEditMessage}
+                  onDeleteMessage={onDeleteMessage}
+                  messages={messages}
+                />
+              </Tab.Pane>
+              <Tab.Pane eventKey={EventKeys.Import}>
+                <JsonMessageImportForm onImportMessage={onImportMessage} />
+              </Tab.Pane>
+            </Tab.Content>
+          </Card.Body>
+        </Tab.Container>
+      </Card>
+    </Container>
+  );
+}
